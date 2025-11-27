68815cb9991581ec9bdf4fb5203c743619d85948
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-11-27 10:57:53 +0300

    Initial commit - Created TourItineraryItem model.

---
+++ app/Models/TourItineraryItem.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TourItineraryItem extends Model
+{
+    protected $fillable = [
+        'tour_id',
+        'time_label',
+        'title',
+        'description',
+        'sort_order',
+    ];
+
+    public function tour(): BelongsTo
+    {
+        return $this->belongsTo(Tour::class);
+    }
+}
