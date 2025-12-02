685f99dedd55f05b1e4b099bf7bfbadd5a760cf6
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-02 07:10:52 +0300

    Initial commit

---
+++ resources/js/toast.js
@@ -0,0 +1,9 @@
+import { Notyf } from "notyf";
+import "notyf/notyf.min.css";
+
+const notyf = new Notyf({
+    duration: 4000,
+    position: { x: "right", y: "top" },
+});
+
+export default notyf;
