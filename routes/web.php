120c13c03d84c1888c3fb09eea32254f43791cc1
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-02 10:42:40 +0300

    Added Auth routes and dashboard access

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\CityController;
 use App\Http\Controllers\TourController;
 use App\Http\Controllers\BookingController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Auth routes
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Protected routes
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
 
 Route::get('/{city:slug}', [CityController::class, 'show'])->name('cities.show');
 
