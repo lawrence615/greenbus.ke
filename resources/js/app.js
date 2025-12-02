92f58fbe116d3e5d836489438640e0efe9ee4341
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-02 07:13:26 +0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import "./bootstrap";
+
+import notyf from "./toast";
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (window.__flashSuccess) {
+        notyf.success(window.__flashSuccess);
+    }
+    if (window.__flashError) {
+        notyf.error(window.__flashError);
+    }
+    if (window.__flashInfo) {
+        notyf.info(window.__flashInfo);
+    }
+});
