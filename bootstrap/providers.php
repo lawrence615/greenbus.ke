d459b07484f621cbbfe4f7a1e1fba4f446c830c0
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-04 07:57:32 +0300

    Added PaymentServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
+    App\Providers\PaymentServiceProvider::class,
 ];
