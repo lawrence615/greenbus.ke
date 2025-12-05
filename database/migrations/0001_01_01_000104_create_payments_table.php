0c89a36179277d143f1581a1a29696ff59674d0a
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-05 04:58:37 +0300

    Set default providers as 'flutterwave'.

--- database/migrations/0001_01_01_000104_create_payments_table.php
+++ database/migrations/0001_01_01_000104_create_payments_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('booking_id')->constrained()->cascadeOnDelete();
-            $table->string('provider')->default('paystack');
+            $table->string('provider')->default('flutterwave');
             $table->string('provider_reference')->nullable();
             $table->unsignedInteger('amount');
             $table->string('currency', 10)->default('KES');
