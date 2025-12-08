a8db3917ac8b52f57b7aa01f53c5d1b2185fd1c4
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-08 11:42:51 +0300

    Added 'code' field in the table.

--- database/migrations/0001_01_01_000100_create_cities_table.php
+++ database/migrations/0001_01_01_000100_create_cities_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('code', 5)->unique()->nullable();
             $table->string('slug')->unique();
             $table->string('country')->default('Kenya');
             $table->boolean('is_active')->default(true);
