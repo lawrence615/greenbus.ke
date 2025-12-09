d4a09b6440c4cd1e627e9d4ed0d7bfbb6d43c0bc
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-09 07:13:26 +0300

    Updated the fillable and hidden array, and added new methods.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -22,6 +22,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'invite_token',
+        'invite_sent_at',
+        'invite_accepted_at',
+        'must_change_password',
     ];
 
     /**
@@ -32,6 +36,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'invite_token',
     ];
 
     /**
@@ -43,7 +48,61 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'invite_sent_at' => 'datetime',
+            'invite_accepted_at' => 'datetime',
+            'must_change_password' => 'boolean',
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * Generate a unique invite token.
+     */
+    public function generateInviteToken(): string
+    {
+        $this->invite_token = Str::random(64);
+        $this->invite_sent_at = now();
+        $this->save();
+
+        return $this->invite_token;
+    }
+
+    /**
+     * Check if the invite is still valid (within 7 days).
+     */
+    public function hasValidInvite(): bool
+    {
+        return $this->invite_token 
+            && $this->invite_sent_at 
+            && $this->invite_sent_at->addDays(7)->isFuture();
+    }
+
+    /**
+     * Check if the user has accepted their invite.
+     */
+    public function hasAcceptedInvite(): bool
+    {
+        return $this->invite_accepted_at !== null;
+    }
+
+    /**
+     * Mark the invite as accepted.
+     */
+    public function acceptInvite(): void
+    {
+        $this->invite_token = null;
+        $this->invite_accepted_at = now();
+        $this->must_change_password = false;
+        $this->email_verified_at = now();
+        $this->save();
+    }
+
+    /**
+     * Check if user needs to change their password.
+     */
+    public function mustChangePassword(): bool
+    {
+        return $this->must_change_password === true;
+    }
+
 }
