eac892814bad88b99a0c3f2db4d5f9000b733c5d
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-10 15:21:05 +0300

    Update created.blade.php

--- resources/views/emails/bookings/created.blade.php
+++ resources/views/emails/bookings/created.blade.php
@@ -17,7 +17,7 @@
                             <tr>
                                 <td style="vertical-align:middle;">
                                     <div style="display:inline-flex;align-items:center;gap:8px;">
-                                        <img src="{{ asset('images/logo.png') }}" alt="Greenbus City Tours" style="width:100px;height:auto;display:block;">
+                                        <img src="{{ asset('images/logo.png') }}" alt="Greenbus City Tours" style="width:32px;height:32px;display:block;">
                                         <div>
                                             <div style="font-weight:600;font-size:15px;">Greenbus City Tours</div>
                                             @if($city)
