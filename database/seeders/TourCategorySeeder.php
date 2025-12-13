9b8d509b8d6794392c6591d76737b470847ea476
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-13 15:32:49 +0300

    Update TourCategorySeeder.php

--- database/seeders/TourCategorySeeder.php
+++ database/seeders/TourCategorySeeder.php
@@ -9,12 +9,38 @@ class TourCategorySeeder extends Seeder
 {
     public function run(): void
     {
-        TourCategory::firstOrCreate(
-            ['slug' => 'single-day-trip'],
+        $categories = [
             [
-                'name' => 'Single day trip',
-                'description' => 'Tours that can be completed within a single day.',
+                'slug' => 'hourly-tours',
+                'name' => 'Hourly Tours',
+                'description' => 'Short tours lasting 1-5 hours',
+                'duration_type' => 'hourly',
             ],
-        );
+            [
+                'slug' => 'half-day-tours',
+                'name' => 'Half Day Tours',
+                'description' => 'Tours lasting approximately 6 hours',
+                'duration_type' => 'half_day',
+            ],
+            [
+                'slug' => 'full-day-tours',
+                'name' => 'Full Day Tours',
+                'description' => 'Full day tours and experiences',
+                'duration_type' => 'full_day',
+            ],
+            [
+                'slug' => 'multiple-day-tours',
+                'name' => 'Multiple Day Tours',
+                'description' => 'Extended tours spanning multiple days',
+                'duration_type' => 'multiple_days',
+            ],
+        ];
+
+        foreach ($categories as $category) {
+            TourCategory::firstOrCreate(
+                ['slug' => $category['slug']],
+                $category
+            );
+        }
     }
 }
