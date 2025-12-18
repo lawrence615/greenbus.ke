ae2b7e566a18d4b849227c75494a76f558ab6e82
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-18 13:39:12 +0300

    Renamed model and specified table.

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Faq extends Model
+class FAQ extends Model
 {
+    protected $table = 'faqs';
+    
     protected $fillable = [
         'question',
         'answer',
