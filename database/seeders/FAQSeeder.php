d516a9e627566f41b18a8f520f615a9e84917b9a
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-18 13:37:59 +0300

    Initial commit - Created a new seeder class.

---
+++ database/seeders/FAQSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Interfaces\FAQRepositoryInterface;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FAQSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(FAQRepositoryInterface $fAQRepositoryInterface): void
+    {
+        $faqs = [
+            [
+                'tour_category_id' => 1,
+                'question' => 'What do i do incase am not able to do the tour or i want a refund?',
+                'answer' => '<p>Kindly write to us through happytribetravel@gmail.com or whatsapp us through +254 726 455 000</p>',
+                'category' => 'Booking',
+                'is_active' => 1,
+                'sort_order' => 1
+            ]
+        ];
+
+        foreach ($faqs as $faq) {
+            $fAQRepositoryInterface->store($faq);
+        }
+    }
+}
