b5d4a0272d5646d7717f0ee998ccbda849b35c59
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-24 11:13:40 +0300

    Initial commit - Created MainController class.

---
+++ app/Http/Controllers/Admin/Tour/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Tour;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
+class MainController extends Controller
+{
+    public function __construct()
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function index()
+    {
+        throw new \Exception('Not implemented');
+    }
+}
