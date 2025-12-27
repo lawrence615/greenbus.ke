c5241cefb4b77159b2c00df842df3e286d9db5e6
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-27 12:05:48 +0300

    Updated the namespace.

--- app/Http/Controllers/Guest/AuthController.php
+++ app/Http/Controllers/Guest/AuthController.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Guest;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
