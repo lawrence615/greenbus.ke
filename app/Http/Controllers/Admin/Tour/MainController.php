85eb330499a46d4a8c6a098fd0b63c7b34adbe58
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-27 12:12:06 +0300

    Implementation is not yet done here.

--- app/Http/Controllers/Admin/Tour/MainController.php
+++ app/Http/Controllers/Admin/Tour/MainController.php
@@ -5,15 +5,31 @@ namespace App\Http\Controllers\Admin\Tour;
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\Tour\MainRepositoryInterface;
 
 class MainController extends Controller
 {
-    public function __construct()
+    public function __construct(private readonly MainRepositoryInterface $mainRepository)
     {
         throw new \Exception('Not implemented');
     }
 
     public function index()
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function show()
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function delete()
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function toggleStatus()
     {
         throw new \Exception('Not implemented');
     }
