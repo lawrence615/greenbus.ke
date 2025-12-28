4e0f696dea275db57ea08c9a9c97f7c33d6a9e75
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-12-28 08:15:55 +0300

    Implemented some of the methods that were not implemented.

--- app/Http/Controllers/Admin/Tour/MainController.php
+++ app/Http/Controllers/Admin/Tour/MainController.php
@@ -6,17 +6,28 @@ use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
 use App\Interfaces\Tour\MainRepositoryInterface;
+use App\Interfaces\Tour\CategoryRepositoryInterface;
+use App\Interfaces\LocationRepositoryInterface;
+use App\Models\Tour;
 
 class MainController extends Controller
 {
-    public function __construct(private readonly MainRepositoryInterface $mainRepository)
-    {
-        throw new \Exception('Not implemented');
-    }
+    public function __construct(
+        private readonly MainRepositoryInterface $mainRepository,
+        private readonly CategoryRepositoryInterface $categoryRepository,
+        private readonly LocationRepositoryInterface $locationRepository,
+    ) {}
 
-    public function index()
+    public function index(Request $request)
     {
-        throw new \Exception('Not implemented');
+        $filters = $request->only(['search', 'location_id', 'category_id', 'status']);
+
+        $locations = $this->locationRepository->getAll();
+        $categories = $this->categoryRepository->getAll();
+
+        $tours = $this->mainRepository->adminIndex($filters);
+
+        return view('admin.tours.index', compact('tours', 'locations', 'categories'));
     }
 
     public function show()
@@ -24,13 +35,22 @@ class MainController extends Controller
         throw new \Exception('Not implemented');
     }
 
-    public function delete()
+    public function destroy(Tour $tour)
     {
-        throw new \Exception('Not implemented');
+        $this->mainRepository->delete($tour);
+
+        return redirect()
+            ->route('console.tours.index')
+            ->with('success', 'Tour deleted successfully.');
     }
 
-    public function toggleStatus()
+    public function toggleStatus(Tour $tour)
     {
-        throw new \Exception('Not implemented');
+        $this->mainRepository->toggleStatus($tour);
+        $status = $tour->status === 'published' ? 'published' : 'unpublished';
+
+        return redirect()
+            ->back()
+            ->with('success', "Tour has been {$status}.");
     }
 }
