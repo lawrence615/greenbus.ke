eb7413b662b73d51fb9356bd87b5c1a7b4a7f28d
Lawrence Macharia <macharialawrence2011@gmail.com>
2026-01-02 16:50:52 +0300

    Update TrashController.php

--- app/Http/Controllers/Admin/Tour/TrashController.php
+++ app/Http/Controllers/Admin/Tour/TrashController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers\Admin\Tour;
 
-use App\Http\Controllers\Controller;
-use App\Models\Tour;
-use App\Repositories\Tour\MainRepository;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Log;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\Tour\MainRepository;
+use App\Interfaces\LocationRepositoryInterface;
+use App\Interfaces\Tour\CategoryRepositoryInterface;
+use App\Models\Tour;
 
 class TrashController extends Controller
 {
     public function __construct(
-        private MainRepository $mainRepository
+        private MainRepository $mainRepository,
+        private LocationRepositoryInterface $locationRepository,
+        private CategoryRepositoryInterface $categoryRepository
     ) {}
 
     /**
@@ -21,17 +27,22 @@ class TrashController extends Controller
     public function index(Request $request): View
     {
         $filters = $request->only(['search', 'location_id', 'category_id']);
+        $locations = $this->locationRepository->getAll();
+        $categories = $this->categoryRepository->getAll();
         $tours = $this->mainRepository->trashed($filters);
+        Log::info('Return all deleted tours: ', compact('tours'));
 
-        return view('admin.tours.trash.index', compact('tours'));
+        return view('admin.tours.trashed.index', compact('tours', 'locations', 'categories'));
     }
 
     /**
      * Restore the specified tour.
      */
-    public function restore(Tour $tour): RedirectResponse
+    public function restore(int $tour): RedirectResponse
     {
-        $this->mainRepository->restore($tour);
+        $tourModel = Tour::withTrashed()->findOrFail($tour);
+        Log::info('Restoring tour: ' . $tourModel->slug);
+        $this->mainRepository->restore($tourModel);
 
         return redirect()
             ->route('console.tours.trash.index')
@@ -41,9 +52,11 @@ class TrashController extends Controller
     /**
      * Permanently delete the specified tour.
      */
-    public function destroy(Tour $tour): RedirectResponse
+    public function destroy(int $tour): RedirectResponse
     {
-        $this->mainRepository->forceDelete($tour);
+        $tourModel = Tour::withTrashed()->findOrFail($tour);
+        Log::info('Permanently deleting tour: ' . $tourModel->slug);
+        $this->mainRepository->forceDelete($tourModel);
 
         return redirect()
             ->route('console.tours.trash.index')
